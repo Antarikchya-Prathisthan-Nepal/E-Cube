88c8ee6bf172ecd94a9a4675971ae2b090e40cbd
Kushal KC <139061932+kushalkessi@users.noreply.github.com>
2025-07-04 01:25:10 +0545

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   base: "/ecube/",
 
   head: [
-    ['link', { rel: 'icon', type: 'image/png', href: '/apn.png' }] 
+    ['link', { rel: 'icon', type: 'image/png', href: '/images/apn.png' }] 
   ],
   
   srcExclude: [
